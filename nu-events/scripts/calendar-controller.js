2d0db98cedaf1638473a0992533fe96c3f5b63c3
Greymouth <thejadenjack@gmail.com>
2017-06-21 11:48:11 -0600

    Added Neumont events

---
+++ nu-events/scripts/calendar-controller.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    googleCalendarApiKey: 'AIzaSyA0UHHgGpba90ybeOMn14jOJB3BvNMTVzY',
+    events: {
+      googleCalendarId: '2kkvqqcmpjf8qg58185hpfqk3s@group.calendar.google.com'
+    }
+  });
+});
